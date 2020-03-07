a46529eae6c4c78aafbfa14630f1471b5a320b0b
Good-Eli <61868766+Good-Eli@users.noreply.github.com>
2020-03-07 18:00:23 +0300

    Update PowerShellScript.ps1

--- vars/PowerShellScript.ps1
+++ vars/PowerShellScript.ps1
@@ -2,6 +2,7 @@ function geturlstat ($url) {
     if ((Test-Connection -computer $url -quiet) -eq $True)
     {$result = "success"}
     Else {$result = "failure"}
+    write-log "$result"
     return $result
     }
 Write-Output " >>> PowerShell function successfully connected from GitHub!"
