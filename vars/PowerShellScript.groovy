e7bd209cd1eff4771f2c49bdf0432c46282a14eb
Good-Eli <61868766+Good-Eli@users.noreply.github.com>
2020-03-07 15:36:15 +0300

    Update PowerShellScript.groovy

--- vars/PowerShellScript.groovy
+++ vars/PowerShellScript.groovy
@@ -2,7 +2,11 @@
 
 def call(body) {
     echo "Start Deploy"
-
+    powershell(script: '''function geturlstat ($url){ 
+                        if ((Test-Connection -computer $url -quiet) -eq $True) {
+                        $result = \"success\"} 
+                        Else {$result = \"failure\"} 
+                        return $result} geturlstat google.com''')
     echo "Deployed"
     currentBuild.result = 'SUCCESS' //FAILURE to fail
 
